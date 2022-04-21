ca7c42aaeaff45d98489b00f167358e313047994
huayemao <huayemao4g@gmail.com>
2022-04-21 17:45:38 +0800

    为 doodle 添加动画

--- components/HeroDoodle.tsx
+++ components/HeroDoodle.tsx
@@ -5,15 +5,35 @@ const HeroDoodle = ({ seed }) => {
     <div
       className="h-32 w-64 overflow-hidden border border-slate-50  md:h-48 md:w-96"
       dangerouslySetInnerHTML={{
-        __html: `<css-doodle seed="${seed}" click-to-update class="doodle">
+        __html: `<css-doodle seed="${seed}" class="doodle">
             :doodle {
-           @grid: 10 / 40em;
-           grid-gap: .4em;
+              @grid: 10 / 40em;
+              grid-gap: .4em;
             }
+
+            :doodle(:hover) {--h:running;}
            
-           --hue: calc(217 + .5 * @row() * @col());
-               background: hsla(var(--hue), 91%, 50%, @r(.1, .9));
-           clip-path: ellipse(100% 100% at @pick('0 0', '0 100%', '100% 0', '100% 100%'));
+            --hue: calc(217 + .5 * @row() * @col());
+            --hue1: calc(217 + .5 * (@row()-1) * (@col()+1));
+            background: hsla(var(--hue), 91%, 50%, @r(.1, .9));
+            clip-path: ellipse(100% 100% at @pick('0 0', '0 100%', '100% 0', '100% 100%'));
+
+            animation: blink 8s infinite;
+            // animation-play-state:var(--h,paused);
+            // animation-delay: @rand(50ms);
+
+            @keyframes blink {
+              0% {
+                background: hsla(var(--hue1), 91%, 50%, @r(.1, .9));
+              }
+              33% {
+                background: hsla(var(--hue), 91%, 50%, @r(.1, .9));
+              }
+              66% {
+                background: hsla(var(--hue1), 91%, 50%, @r(.1, .9));
+              }
+            }
+
            </css-doodle>`,
       }}
     />
