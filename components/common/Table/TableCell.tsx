ca9d19238bb3c5884ab44977b41c9a62535483a0
huayemao <huayemao4g@gmail.com>
2022-06-10 22:52:11 +0800

    Table 组件

---
+++ components/common/Table/TableCell.tsx
@@ -0,0 +1,12 @@
+import classNames from 'clsx';
+import type { ComponentProps, FC, PropsWithChildren } from 'react';
+
+export type TableCellProps = PropsWithChildren<ComponentProps<'td'>>;
+
+export const TableCell: FC<TableCellProps> = ({ children, className, ...props }) => {
+  return (
+    <td className={classNames('px-6 py-4', className)} {...props}>
+      {children}
+    </td>
+  );
+};
---
+++ components/common/Table/TableHeadCell.tsx
@@ -0,0 +1,12 @@
+import classNames from 'clsx';
+import type { ComponentProps, FC, PropsWithChildren } from 'react';
+
+export type TableHeadCellProps = PropsWithChildren<ComponentProps<'th'>>;
+
+export const TableHeadCell: FC<TableHeadCellProps> = ({ children, className, ...props }) => {
+  return (
+    <th className={classNames('px-6 py-3', className)} {...props}>
+      {children}
+    </th>
+  );
+};
