342529565721e2677aa21ef6ff33df1ba44ea336
huayemao <huayemao4g@gmail.com>
2023-08-08 20:24:38 +0800

    fix: enable 移动端底导航主页按钮

--- app/BottomTab.tsx
+++ app/BottomTab.tsx
@@ -1,20 +1,23 @@
 import {
-  MagnifyingGlassIcon,
   Cog8ToothIcon,
   HomeIcon,
+  MagnifyingGlassIcon
 } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 
 export const BottomTab = () => {
   return (
     <div className="fixed left-4 right-4 bottom-4 z-10 mx-auto flex h-12 items-center justify-between rounded-lg shadow backdrop-blur md:hidden">
-      <div className="flex flex-1 justify-center text-center">
+      <Link href={'/'} className="flex flex-1 justify-center text-center">
         <HomeIcon className="h-8 w-8 text-slate-600" />
-      </div>
+      </Link>
       <Link href={'/search'} className="flex flex-1 justify-center">
         <MagnifyingGlassIcon className="h-8 w-8 text-slate-600" />
       </Link>
-      <div className="flex flex-1 justify-center">
+      <div
+        // onClick={() => alert('这个按钮暂时还没有什么用哦')}
+        className="flex flex-1 justify-center"
+      >
         <Cog8ToothIcon className="h-8 w-8 text-slate-600" />
       </div>
     </div>
